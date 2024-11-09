0ecdb3c3c92ba98a17e18492ba84c46e136aa55f
Harsh Gill <112195414+349gill@users.noreply.github.com>
2024-11-09 14:23:12 -0700

    DB Added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
